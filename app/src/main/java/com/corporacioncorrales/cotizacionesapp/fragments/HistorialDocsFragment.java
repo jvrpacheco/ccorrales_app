78f7cb44cc1fd63bb74f9090165aa850256d81a8
victor <victor.espinoza.garcia@gmail.com>
2016-10-04 03:58:09 -0500

    Filtros Historial.

--- app/src/main/java/com/corporacioncorrales/cotizacionesapp/fragments/HistorialDocsFragment.java
+++ app/src/main/java/com/corporacioncorrales/cotizacionesapp/fragments/HistorialDocsFragment.java
@@ -1,6 +1,7 @@
 package com.corporacioncorrales.cotizacionesapp.fragments;
 
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,11 +10,17 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.corporacioncorrales.cotizacionesapp.R;
 import com.corporacioncorrales.cotizacionesapp.activities.MainActivity;
-import com.corporacioncorrales.cotizacionesapp.adapters.ClientsAdapter;
 import com.corporacioncorrales.cotizacionesapp.adapters.DocumentsAdapter;
 import com.corporacioncorrales.cotizacionesapp.model.DocumentsResponse;
 import com.corporacioncorrales.cotizacionesapp.networking.DocumentsApi;
@@ -21,10 +28,14 @@ import com.corporacioncorrales.cotizacionesapp.utils.Common;
 import com.corporacioncorrales.cotizacionesapp.utils.Constants;
 import com.corporacioncorrales.cotizacionesapp.utils.Singleton;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -38,9 +49,30 @@ public class HistorialDocsFragment extends Fragment {
 
     @BindView(R.id.rvDocumentsHistory)
     RecyclerView rvDocumentsHistory;
+    @BindView(R.id.rl_btnFechaInicial)
+    RelativeLayout rlBtnFechaInicial;
+    @BindView(R.id.rl_btnFechaFinal)
+    RelativeLayout rlBtnFechaFinal;
+    @BindView(R.id.tvFechaInicial)
+    TextView tvFechaInicial;
+    @BindView(R.id.tvFechaFinal)
+    TextView tvFechaFinal;
+    @BindView(R.id.spRubroDoc)
+    Spinner spRubroDoc;
+    @BindView(R.id.spEstadoDoc)
+    Spinner spEstadoDoc;
+    @BindView(R.id.btnFiltrarDocumentos)
+    Button btnFiltrarDocumentos;
     private ProgressBar mainProgressBar;
     private ArrayList<DocumentsResponse> documentsArrayList;
     private DocumentsAdapter documentsAdapter;
+    private int mYearInicial, mMonthInicial, mDayInicial, mYearFinal, mMonthFinal, mDayFinal;
+    private String fechaInicial;
+    private String fechaFinal;
+    private String estadoDocSeleccionado;
+    private String rubroSeleccionado;
+    private SimpleDateFormat formatToShow;
+    private SimpleDateFormat formatToSend;
 
     public HistorialDocsFragment() {
         // Required empty public constructor
@@ -51,10 +83,19 @@ public class HistorialDocsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        View view = inflater.inflate(R.layout.fragment_historial_docs, container, false);
+        ButterKnife.bind(this, view);
+
         Common.setActionBarTitle(getActivity(), Constants.fragmentTagHistorial);
         Singleton sg = Singleton.getInstance();
         mainProgressBar = ((MainActivity) getActivity()).mProgressBar;
         documentsArrayList = new ArrayList<>();
+        formatToShow = new SimpleDateFormat("dd/MM/yyyy");
+        formatToSend = new SimpleDateFormat("yyyyMMdd"); //20161015
+
+        initDates();
+        initSpinnerEstadosDoc();
+        initSpinnerRubroDoc();
 
         getDocumentsHistory(sg.getUserCode(),
                 "0",  //sg.getIdclientSelected()
@@ -70,8 +111,6 @@ public class HistorialDocsFragment extends Fragment {
                 "20160701",
                 "20160931");*/
 
-        View view = inflater.inflate(R.layout.fragment_historial_docs, container, false);
-        ButterKnife.bind(this, view);
         return view;
     }
 
@@ -123,5 +162,165 @@ public class HistorialDocsFragment extends Fragment {
             }
         });
 
+    }
+
+    @OnClick(R.id.btnFiltrarDocumentos)
+    public void OnClickFiltrarDocumentos() {
+        Log.d(Constants.log_arrow, String.format("Fecha inicial: %s, Fecha final: %s, Estado documento: %s, Rubro: %s", fechaInicial, fechaFinal, estadoDocSeleccionado, rubroSeleccionado));
+    }
+
+    @OnClick(R.id.rl_btnFechaInicial)
+    public void OnClickFechaInicial() {
+        // Get Current Date
+        final Calendar c = Calendar.getInstance();
+        mYearInicial = c.get(Calendar.YEAR);
+        mMonthInicial = c.get(Calendar.MONTH);
+        mDayInicial = c.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerFechaInicialDialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                fechaInicial = formatToSend.format(new Date(year, monthOfYear, dayOfMonth));
+                tvFechaInicial.setText(formatToShow.format(new Date(year, monthOfYear, dayOfMonth)));
+            }
+        }, mYearInicial, mMonthInicial, mDayInicial);
+
+        datePickerFechaInicialDialog.setCancelable(false);
+        datePickerFechaInicialDialog.show();
+    }
+
+
+    @OnClick(R.id.rl_btnFechaFinal)
+    public void OnClickFechaFinal() {
+        final Calendar c = Calendar.getInstance();
+        mYearFinal = c.get(Calendar.YEAR);
+        mMonthFinal = c.get(Calendar.MONTH);
+        mDayFinal = c.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerFechaFinalDialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                //fechaFinal = dayOfMonth + "/" + (monthOfYear + 1) + "/" + year;
+                //tvFechaFinal.setText(fechaFinal);
+
+                //fechaFinal = formatToSend.format(new Date(year, monthOfYear, dayOfMonth));
+                //tvFechaFinal.setText(formatToShow.format(new Date(year, monthOfYear, dayOfMonth)));
+
+                tvFechaFinal.setText(formatDate(year, monthOfYear, dayOfMonth));
+            }
+        }, mYearFinal, mMonthFinal, mDayFinal);
+
+        datePickerFechaFinalDialog.setCancelable(false);
+        datePickerFechaFinalDialog.show();
+    }
+
+    private static String formatDate(int year, int month, int day) {
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(0);
+        cal.set(year, month, day);
+        Date date = cal.getTime();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        return sdf.format(date);
+    }
+
+    private String getCurrentDateToSend() {
+        String currentDate = Constants.Empty;
+        try {
+            Date curDate = new Date();
+            currentDate = formatToSend.format(curDate);
+
+        } catch (Exception e) {
+            Log.e(Constants.log_arrow_error, e.toString());
+        }
+        return currentDate;
+    }
+
+    private String getCurrentDateToShow() {
+        String currentDate = Constants.Empty;
+        try {
+            Date curDate = new Date();
+            currentDate = formatToShow.format(curDate);
+
+        } catch (Exception e) {
+            Log.e(Constants.log_arrow_error, e.toString());
+        }
+        return currentDate;
+    }
+
+    private String getCurrentDate1() {
+        int day, month, year;
+        String currentDate = Constants.Empty;
+        try {
+            final Calendar c = Calendar.getInstance();
+            year = c.get(Calendar.YEAR);
+            month = c.get(Calendar.MONTH);
+            day = c.get(Calendar.DAY_OF_MONTH);
+            currentDate = day + "/" + (month + 1) + "/" + year;
+
+        } catch (Exception e) {
+            Log.e(Constants.log_arrow_error, e.toString());
+        }
+        return currentDate;
+    }
+
+    private void initDates() {
+        fechaInicial = getCurrentDateToShow();
+        fechaFinal = getCurrentDateToShow();
+        tvFechaInicial.setText(fechaInicial);
+        tvFechaFinal.setText(fechaFinal);
+    }
+
+    private void initSpinnerEstadosDoc() {
+        ArrayAdapter adapterRubroType = ArrayAdapter.createFromResource(getActivity(), R.array.array_estados_doc, android.R.layout.simple_list_item_1);
+        spEstadoDoc.setAdapter(adapterRubroType);
+        spEstadoDoc.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                if (item.equals(Constants.estadoDoc_todos_label)) {
+                    estadoDocSeleccionado = Constants.estadoDoc_todos;
+                } else if (item.equals(Constants.estadoDoc_aceptados_label)) {
+                    estadoDocSeleccionado = Constants.estadoDoc_aceptados;
+                } else if (item.equals(Constants.estadoDoc_pendientes_label)) {
+                    estadoDocSeleccionado = Constants.estadoDoc_pendientes;
+                } else if (item.equals(Constants.estadoDoc_rechazados_label)) {
+                    estadoDocSeleccionado = Constants.estadoDoc_rechazados;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private void initSpinnerRubroDoc() {
+        ArrayAdapter adapterRubroType = ArrayAdapter.createFromResource(getActivity(), R.array.array_rubros_historial, android.R.layout.simple_list_item_1);
+        spRubroDoc.setAdapter(adapterRubroType);
+        spRubroDoc.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                if (item.equals(Constants.rubro_todos_label)) {
+                    rubroSeleccionado = Constants.rubro_todos;
+                } else if (item.equals(Constants.rubro_vidrio_label)) {
+                    rubroSeleccionado = Constants.rubro_vidrio;
+                } else if (item.equals(Constants.rubro_aluminio_label)) {
+                    rubroSeleccionado = Constants.rubro_aluminio;
+                } else if (item.equals(Constants.rubro_accesorio_label)) {
+                    rubroSeleccionado = Constants.rubro_accesorio;
+                } else if (item.equals(Constants.rubro_plastico_label)) {
+                    rubroSeleccionado = Constants.rubro_plastico;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/corporacioncorrales/cotizacionesapp/utils/Constants.java
+++ app/src/main/java/com/corporacioncorrales/cotizacionesapp/utils/Constants.java
@@ -10,10 +10,12 @@ public class Constants {
     public static final String allowEnterToApp = "True";
     public static final String denyEnterToApp = "False";
 
+
     public static final String log_arrow = "--------> ";
     public static final String log_arrow_error = "--------> ERROR";
     public static final String log_arrow_response = "--------> RESPONSE";
     public static final String log_arrow_failure = "--------> FAILURE";
+
 
     public static final String url_server = "http://190.81.34.42:8080/";
     public static final String url_user_login = "api/usuario/{user}";
@@ -25,6 +27,7 @@ public class Constants {
     public static final String url_documents_history = "api/consultaproforma/historialproforma";
     public static final String url_documents_history_detail = "api/consultaproformadetalle";
 
+
     public static final String rubro_aluminio_label = "Aluminio";
     public static final String rubro_aluminio = "00";
     public static final String rubro_vidrio_label = "Vidrio";
@@ -33,7 +36,9 @@ public class Constants {
     public static final String rubro_accesorio = "03";
     public static final String rubro_plastico_label = "Plastico";
     public static final String rubro_plastico = "04";
+    public static final String rubro_todos_label = "Todos";
     public static final String rubro_todos = "99";
+
 
     public static final String tipoDoc_factura_label = "Factura";
     //public static final String tipoDoc_factura = "00";
@@ -43,15 +48,22 @@ public class Constants {
     public static final String tipoDoc_preventa_label = "Preventa";
     public static final String tipoDoc_preventa = "3";
 
-    public static final String estadoDoc_pendientes = "1";
-    public static final String estadoDoc_aprobados = "2";
-    public static final String estadoDoc_rechazados = "3";
+
+    public static final String estadoDoc_todos_label = "Todos";
     public static final String estadoDoc_todos = "0";
+    public static final String estadoDoc_aceptados_label = "Aceptados";
+    public static final String estadoDoc_aceptados = "1";
+    public static final String estadoDoc_pendientes_label = "Pendientes";
+    public static final String estadoDoc_pendientes = "2";
+    public static final String estadoDoc_rechazados_label = "Rechazados";
+    public static final String estadoDoc_rechazados = "3";
+
 
     //para comparar los precios
     public static final String comparar_esMayor = "esMayor";
     public static final String comparar_esMenor = "esMenor";
     public static final String comparar_esIgual = "esIgual";
+
 
     public static final String idClienteHeader = "idCliente";
     public static final String idRubroHeader = "idRubro";
@@ -64,15 +76,17 @@ public class Constants {
     public static final String idFechaInicioDocHeader = "fecini";
     public static final String idFechaFinDocHeader = "fecfin";
 
+
     public static final String montoTotalMayorALineaDeCredito = "1";
     public static final String montoTotalMenorOIgualALineaDeCredito = "0";
-
     public static final String dentroDeLineaDeCredito = "Dentro de la Linea de Credito";
     public static final String superaLineaDeCredito = "Supera Linea de Credito";
+
 
     public static final String fragmentTagClientes = "Clientes";
     public static final String fragmentTagProductos = "Productos";
     public static final String fragmentTagHistorial = "Historial";
+
 
     public static final String round_two_decimals = "%.2f";
     public static final String round_three_decimals = "%.3f";
